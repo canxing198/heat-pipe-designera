9f20862e2bfea0e8f5244b63708ef1e02b301ae5
canxing198 <liujiang@novark.com.cn>
2026-05-08 20:22:15 +0800

    Delete excel_export.py

--- excel_export.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from datetime import datetime
-
-def export_excel(res):
-    df = pd.DataFrame([res])
-    fname = f"HP_Design_{datetime.now():%Y%m%d_%H%M%S}.xlsx"
-    df.to_excel(fname, index=False)
